acab1ea6393b79b092c5980c9fd3f14d7a149be0
Justin Miller <jmiller9@gmail.com>
2013-04-16 17:55:59 -0400

    HW complete for Exercise 27.3 dist web systems Spring 2013

---
+++ web/resources/js/util.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function show() {   
+            document.getElementById("addressForm").style.display="inline";
+          };
+
+function hide() {   
+            document.getElementById("addressForm").style.display="none";
+          };
